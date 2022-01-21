beb9524e7d34dfe85c0fd8e76723ef151f815105
saddammd <saddam.md@gmail.com>
2022-01-21 06:41:41 +0530

    added loggers

--- src/main/java/com/ntt/poc/utilities/AuthenticationUtility.java
+++ src/main/java/com/ntt/poc/utilities/AuthenticationUtility.java
@@ -58,16 +58,19 @@ public class AuthenticationUtility  {
 
 	public UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request) {
 
+		logger.info("signed in with JWT token");
 		String headers = request.getHeader(HttpHeaders.AUTHORIZATION);
 		String token = headers.replace("Bearer", "");
 		Claims claims = Jwts.parser().setSigningKey("1nc4jRjdO5enfUc4loN3q7gEb8fhr9O").parseClaimsJws(token).getBody();
 		String id = Jwts.parser().setSigningKey("1nc4jRjdO5enfUc4loN3q7gEb8fhr9O").parseClaimsJws(token).getBody()
 				.getSubject();
+		logger.info("decoded JWT Token");
 
 		Set<SimpleGrantedAuthority> authorities = new HashSet<>();
 
 		String args[] = claims.get("Role").toString().split(",");
 
+		logger.info("trimming roles from JWT token");
 		for (String roles : args) {
 
 			String str1 = roles.replaceAll("\\[", "").trim();
@@ -75,14 +78,13 @@ public class AuthenticationUtility  {
 			String role = "ROLE_".concat(str2);
 			authorities.add(new SimpleGrantedAuthority(role));
 
-			logger.info(role);
+			logger.info("role" +role +"has trimmed from JWT Token");
 		}
 
 		Collection<? extends GrantedAuthority> authorities2 = java.util.Arrays.asList(args).stream()
 				.map(authority -> new SimpleGrantedAuthority(authority)).collect(Collectors.toList());
 
-		// return new UsernamePasswordAuthenticationToken(claims.getSubject(),
-		// request.getHeader(HttpHeaders.AUTHORIZATION),authorities);
+		logger.info("passing authentication object");
 		return new UsernamePasswordAuthenticationToken(claims.getSubject(),
 				request.getHeader(HttpHeaders.AUTHORIZATION), authorities);
 
@@ -90,7 +92,7 @@ public class AuthenticationUtility  {
 	
 	
 	public UsernamePasswordAuthenticationToken getGoogleAuthentication(HttpServletRequest request) throws IOException {
-		
+		logger.info("Signined in with Google token");
 		System.out.println(environment.getProperty("google.clientId"));
 		 final NetHttpTransport transport = new NetHttpTransport();
 	        final JacksonFactory jacksonFactory = JacksonFactory.getDefaultInstance();
@@ -100,10 +102,13 @@ public class AuthenticationUtility  {
 	        
 	        String headers = request.getHeader(HttpHeaders.AUTHORIZATION);
 			String token = headers.replace("Bearer", "");
+			logger.info("removing bearer from google token");
 	        final GoogleIdToken googleIdToken = GoogleIdToken.parse(verifier.getJsonFactory(), token);
 	        final GoogleIdToken.Payload payload = googleIdToken.getPayload();
 	        
+	        logger.info("finding the user in database, if not found user will be registered");
 	        if(this.user_Repository.findByEmail(payload.getEmail())==null) {
+	        	logger.info("user details not found in database so proceeding with registration");
 	        	User user2 = new User();
 	    		user2.setEmail(payload.getEmail());
 	    		user2.setName(payload.getEmail());
@@ -114,31 +119,30 @@ public class AuthenticationUtility  {
 	    		Roles role1 = this.roles_Repository.getRoleByRoleId(3);
 	    		user2.getRoles().add(role1);
 	    		this.user_Repository.save(user2);
+	    		logger.info("a new user registered with Google email Id");
 	    	 }
 	        
 	        User findUser = this.user_Repository.findByEmail(payload.getEmail());
+	        logger.info("user has found in database");
 
 	        Set<SimpleGrantedAuthority> authorities = new HashSet<>();
 	    	Roles roles = new Roles();
 	    	
 	    	int size = findUser.getRoles().size();
-	    	
+	    	logger.info("getting roles details of user");
 	    		for (int i=0; i<=size-1; i++) {
 	    	
 	    	roles = findUser.getRoles().get(i);
 	    	String str = roles.getAuthority();
-	    	
+	    	logger.info("trimming roles details from database");
 	    	String str1 = str.replaceAll("\\[", "").trim();
 	    	String str2 = str1.replaceAll("\\]", "").trim();
 	    	String role = "ROLE_".concat(str2);
 	    	authorities.add(new SimpleGrantedAuthority(role));
 
-	    			logger.info(role);
+	    			logger.info("role value after trimming is " +role);
 	    		}
-
-//	    		Collection<? extends GrantedAuthority> authorities2 = java.util.Arrays.asList(args).stream()
-//	    				.map(authority -> new SimpleGrantedAuthority(authority)).collect(Collectors.toList());
-
+	    		logger.info("allocating the user email and roles to authentication object");
 	    		return new UsernamePasswordAuthenticationToken(findUser.getEmail(),
 	    				request.getHeader(HttpHeaders.AUTHORIZATION), authorities);
 
